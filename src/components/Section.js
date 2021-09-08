75e157fd186365536aef484c3fae18b2a8daa0b1
Ignacio Cl <juanigcl2532@gmail.com>
2021-09-07 23:32:41 -0600

    Adding backup of tesla

---
+++ src/components/Section.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
+// props son los parametros que se pueden pasar como parametros desde home.js
+function Section({title , description , LeftBtnText, rightBtnText, backgroundImg}) {
+    return (
+        <Wrap bgImage = {backgroundImg}>
+            <Fade bottom>
+                <ItemText>
+                    <h1> {title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
+            <Buttons>
+                <Fade bottom> 
+                    <ButtonGroup>
+                        <LeftButton>
+                            {LeftBtnText}
+                        </LeftButton>
+                        {rightBtnText &&  // Esto es un condicional y se lee asi
+                        // Si el boton derecho existe esos && son then entonces  ponga eso
+                        // y eso verifica en home.js en los props 
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                    <DownArrow src = "images/down-arrow.svg"/> 
+                </Fade>
+            </Buttons>
+        </Wrap>
+    )
+};
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size:cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between; 
+    align-items:center;
+    background-image: ${props => `url("/images/${props.bgImage}")`}
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align:center;
+    z-index: 10;
+`
+
+const ButtonGroup = styled.div`
+    display:flex;
+    margin-bottom: 30px;
+    @media(max-width: 768px) {
+        flex-direction:column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color:rgba(23, 26, 32, 0.8);
+    height:40px;
+    width:256px;
+    color:white;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform:uppercase;
+    font-size: 12px;
+    cursor:pointer;
+    margin:8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background:white;
+    opacity:0.65;
+    color:black;
+`
+
+// Al hacer un styled(leftButton) le estamos diciendo que agregue los estilos de leftButton y los agregue tambien en right button 
+
+const DownArrow = styled.img`
+    height:40px;
+    overflow-x: hidden;
+    animation:animateDown infinite 1.5s;
+`
+
+
+const Buttons = styled.div`
+
+`
